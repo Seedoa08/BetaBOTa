88a705d5f224f8eb219ca2b1cd79dcc46cf6dd23
Seedoa404 <rayan.farhatyo@gmail.com>
2025-04-12 18:50:31 +0100

    Update automod.js

--- utils/automod.js
+++ utils/automod.js
@@ -1,9 +1,10 @@
-const { OpenAIApi } = require('openai');
+const { Configuration, OpenAIApi } = require('openai');
 require('dotenv').config();
 
-const openai = new OpenAIApi({
-    apiKey: process.env.OPENAI_API_KEY // Remplacez par votre clé API OpenAI
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
 });
+const openai = new OpenAIApi(configuration);
 
 async function analyze(message) {
     try {
